27dbf952187c0f21cf8bcfcbcef67db8af0682fa
jacquesradjabu <jacquesradjabu@gmail.com>
2024-07-15 12:17:07 +0100

    add netlily flag for deployment

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,43 +1,43 @@
-/**
- * @license
- * Copyright 2024 Birusha Ndegeya, sofia and Contributors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-import { ReactNode } from "react";
-import type { Metadata } from "next";
-import Sidebar from "@/components/Sidebar";
-type HomeLayoutProps = {
-   children: ReactNode;
-}
+// /**
+//  * @license
+//  * Copyright 2024 Birusha Ndegeya, sofia and Contributors
+//  *
+//  * Licensed under the Apache License, Version 2.0 (the "License");
+//  * you may not use this file except in compliance with the License.
+//  * You may obtain a copy of the License at
+//  *
+//  *   http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  * Unless required by applicable law or agreed to in writing, software
+//  * distributed under the License is distributed on an "AS IS" BASIS,
+//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  * See the License for the specific language governing permissions and
+//  * limitations under the License.
+//  */
+// import { ReactNode } from "react";
+// import type { Metadata } from "next";
+// import Sidebar from "@/components/Sidebar";
+// type HomeLayoutProps = {
+//    children: ReactNode;
+// }
 
-export const metadata: Metadata = {
-   title: "home",
-   description: "inbox chat social media reseau social home page page d'acceuil",
-};
+// export const metadata: Metadata = {
+//    title: "home",
+//    description: "inbox chat social media reseau social home page page d'acceuil",
+// };
 
 
-export default function RootLayout({ children }: Readonly<HomeLayoutProps>) {
-   return (
-      <html lang="en">
-         <body className={`h-screen flex items-center justify-center`}>
-            <div className="border border-gray-200 w-[100%] h-[100%] flex overflow-hidden">
-               <Sidebar />
-               <div className="flex-1 hidden md:block">
-                  {children}
-               </div>
-            </div>
-         </body>
-      </html>
-   );
-}
+// export default function RootLayout({ children }: Readonly<HomeLayoutProps>) {
+//    return (
+//       <html lang="en">
+//          <body className={`h-screen flex items-center justify-center`}>
+//             <div className="border border-gray-200 w-[100%] h-[100%] flex overflow-hidden">
+//                <Sidebar />
+//                <div className="flex-1 hidden md:block">
+//                   {children}
+//                </div>
+//             </div>
+//          </body>
+//       </html>
+//    );
+// }
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,26 +1,26 @@
-'use client';
-import { useContext } from "react";
-import Message from "@/components/Message";
-// import SearchBar from "@/components/SearchBar";
-import { data } from "@/data/message";
-import Link from "next/link";
+// 'use client';
+// import { useContext } from "react";
+// import Message from "@/components/Message";
+// // import SearchBar from "@/components/SearchBar";
+// import { data } from "@/data/message";
+// import Link from "next/link";
 
 
-export default function Home() {
-   return (
+// export default function Home() {
+//    return (
 
-      <div className="">
-         {/* <SearchBar /> */}
-         <div className="flex p-3 flex-col gap-3 overflow-scroll">
-            {
-               data.map((item, i) => (
-                  <Link key={i} href={`/home/${item.name}`}>
-                     <Message name={item.name} message={item.messages.reverse()[0]} avatar={item.avatar} />
-                  </Link>
-               ))
-            }
-         </div>
-      </div>
+//       <div className="">
+//          {/* <SearchBar /> */}
+//          <div className="flex p-3 flex-col gap-3 overflow-scroll">
+//             {
+//                data.map((item, i) => (
+//                   <Link key={i} href={`/home/${item.name}`}>
+//                      <Message name={item.name} message={item.messages.reverse()[0]} avatar={item.avatar} />
+//                   </Link>
+//                ))
+//             }
+//          </div>
+//       </div>
 
-   );
-}
+//    );
+// }
